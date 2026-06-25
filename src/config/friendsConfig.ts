e5e0869b33a5197d14191107023f47c5e08c7fc8
aTaKa-YX <2387900629@qq.com>
2026-06-25 17:12:23 +0800

    友链模块开启

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -4,35 +4,27 @@ import type { FriendLink } from "../types/config";
 
 // 友链配置
 export const friendsConfig: FriendLink[] = [
-	/*
+	
 	{
-		title: "夏夜流萤",
-		imgurl: "https://q1.qlogo.cn/g?b=qq&nk=7618557&s=640",
-		desc: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
-		siteurl: "https://blog.cuteleaf.cn",
+		title: "今朝酒",
+		imgurl: "https://www.kesazake.top/assets/images/%E8%8A%B1%E3%82%88%E3%82%8A%E5%BC%BE%E4%B8%B8.avif",
+		desc: "你好，我是今朝酒。",
+		siteurl: "https://www.kesazake.top/",
 		tags: ["Blog"],
 		weight: 10, // 权重，数字越大排序越靠前
 		enabled: true, // 是否启用
 	},
+
 	{
-		title: "Firefly Docs",
-		imgurl: "https://docs-firefly.cuteleaf.cn/logo.png",
-		desc: "Firefly主题模板文档",
-		siteurl: "https://docs-firefly.cuteleaf.cn",
-		tags: ["Docs"],
-		weight: 9,
-		enabled: true,
+		title: "孤蛍",
+		imgurl: "https://www.kohotaru.top/_astro/my_avatar.Cr_QqJX1_1LiwTq.webp",
+		desc: "欢迎来到我的秘密小屋",
+		siteurl: "https://www.kohotaru.top/",
+		tags: ["Blog"],
+		weight: 9, // 权重，数字越大排序越靠前
+		enabled: true, // 是否启用
 	},
-	{
-		title: "Astro",
-		imgurl: "https://avatars.githubusercontent.com/u/44914786?v=4&s=640",
-		desc: "The web framework for content-driven websites. ⭐️ Star to support our work!",
-		siteurl: "https://github.com/withastro/astro",
-		tags: ["Framework"],
-		weight: 8,
-		enabled: true,
-	},
-	*/
+	
 ];
 
 // 获取启用的友链并按权重排序
