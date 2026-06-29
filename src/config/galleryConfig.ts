e5bacc78710691433249cc15557e5efa15e9edb5
aTaKa-YX <2387900629@qq.com>
2026-06-30 00:36:56 +0800

    照片配置

--- src/config/galleryConfig.ts
+++ src/config/galleryConfig.ts
@@ -1,6 +1,15 @@
 import type { GalleryConfig } from "@/types/galleryConfig";
 
 export const galleryConfig: GalleryConfig = {
-	albums: [],
+	albums: [
+		{
+			id: "my-album",
+			name: "我的相册",
+			description: "描述文字",
+			date: "2026-06-30",
+			location: "波音",
+			tags: ["漫画"],
+		},
+	],
 	columnWidth: 240,
 };
